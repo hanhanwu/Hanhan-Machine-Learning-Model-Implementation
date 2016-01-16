4c6453d8a3bb7c76eae371d78a8b00569932f9aa
hanhan <wuhanhan999@gmail.com>
2016-01-15 18:20:23 -0800

    update optimization_alg.py

--- optimization_alg.py
+++ optimization_alg.py
@@ -7,17 +7,24 @@ this algorithm is willing to accept a worse solution near the beginning of the p
 but later will become less and less likely to accept worse solution,
 till the end, it only accepts better solutions
 '''
-from random import random, randint
+import random as rd
+import math
+import heterogeneous_data
+import mock_Chinese_stock_price
+import cross_validation
+import KNN
 
-def annealing_opt(domain, costf, T = 10000.0, cool = 0.95, step = 1):
+scale = [10, 10, 0.00001, 0]
+
+def annealing_opt(domain, costf, T = 10000.0, cool = 0.95, step = 5):
     # random initialize the Vector
-    vec = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
+    vec = [rd.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
     
     # randomly choose a direction
-    dir = random.randint(-step, step)
+    dir = rd.randint(-step, step)
     
     # randomly choose an index
-    i = random.randint(0, len(domain)-1)
+    i = rd.randint(0, len(domain)-1)
     
     while T > 0.1:
         vec_new = vec
@@ -28,12 +35,29 @@ def annealing_opt(domain, costf, T = 10000.0, cool = 0.95, step = 1):
         error_new = costf(vec_new)
         p = pow(math.e, (-error_new-error_old)/T)
         
-        if (error_new < error_old or random.random() < p):
+        if (error_new < error_old or rd.random() < p):
             vec = vec_new
             
         T *= cool
+        print T
     
-    return vec
-        
-        
+    return vec 
+
+# return the cost function
+def generatecostf(data, algr, trails):
+    def costf(scale):
+        rescaled_data = heterogeneous_data.rescale(data, scale)
+        cost = cross_validation.cross_validate(data, algr, trails)
+        return cost
+    return costf
+
+
+def main():
+    heterogeneous_data, min_max = mock_Chinese_stock_price.get_stockset_various()
+    costf = generatecostf(heterogeneous_data, algr=KNN.get_KNN, trails=10)
+    annealing_optimized_result = annealing_opt(min_max, costf)
+    print 'using annealing optimizaton: [rating, age, duration, investment, employee_number]', annealing_optimized_result
+    
+if __name__ == '__main__':
+    main()
     
