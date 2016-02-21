dad91407071f8780f52fccc0eac558aece7e4eb6
hanhan <wuhanhan999@gmail.com>
2016-02-20 21:16:26 -0800

    update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,17 +1,26 @@
 '''
 Created on Feb 13, 2016
 @author: hanhanwu
+Using sqlite3 as database
+Download sqlite here: http://www.sqlite.org/download.html
+Opern your terminal, cd to the sqlite folder, type "sqlite3"
 '''
+from urllib2 import urlopen
+from bs4 import BeautifulSoup
+from urlparse import urljoin
+from sets import Set
+from sqlite3 import dbapi2 as sqlite
+
 
 class crawler:
     def __init__(self, dbname):
-        pass
+        self.con = sqlite.connect(dbname)
     
     def __del__(self):
-        pass
+        self.con.close()
     
-    def __dbcommit__(self):
-        pass
+    def dbcommit(self):
+        self.con.commit()
     
     def get_entry_id(self, table, field, value, create_new=True):
         return None
@@ -40,5 +49,28 @@ class crawler:
     
     # create database tables
     def create_index_tables(self):
-        pass
+        self.con.execute('create table if not exists urllist(url)')
+        self.con.execute('create table if not exists wordlist(word)')
+        self.con.execute('create table if not exists wordlocation(urlid, wordid, location)')
+        self.con.execute('create table if not exists link(fromid integer, toid integer)')
+        self.con.execute('create table if not exists linkwords(wordid, linkid)')
+         
+        self.con.execute('create index if not exists wordidx on wordlist(word)')
+        self.con.execute('create index if not exists urlidx on urllist(url)')
+        self.con.execute('create index if not exists wordurlidx on wordlocation(wordid)')
+        self.con.execute('create index if not exists urltoidx on link(toid)')
+        self.con.execute('create index if not exists urlfromidx on link(fromid)')
+        
+        self.dbcommit()
     
+    
+def main():
+    ignorewords = Set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
+    
+    # create tables and the indexes
+    dbname = 'searchindex.db'
+    mycrawler = crawler(dbname)
+    mycrawler.create_index_tables()
+        
+if __name__ == '__main__':
+    main()
