269a72a77058a00ce70a2291e906f0a0fe5b110b
hanhan <wuhanhan999@gmail.com>
2016-09-11 22:53:09 -0700

    update genetic_alg.general.py

--- genetic_alg.general.py
+++ genetic_alg.general.py
@@ -1,24 +1,49 @@
 '''
-Created on Sep 8, 2016
+Created on Sep 11, 2016
 '''
 
 import random
 
-def mutation(solution_vec, domain, step):
-    random_idx = random.randint(0, len(solution_vec)-1)
-    if random.random() < 0.5 and solution_vec[random_idx] > domain[random_idx][0]:
-        solution_vec[random_idx] -= step
-    elif solution_vec[random_idx] < domain[random_idx][1]:
-        solution_vec[random_idx] += step
-    return solution_vec
 
-
-def crossover(svec1, svec2):
-    random_idx = random.randint(1, len(svec1)-2)
-    return svec1[0:random_idx]+svec2[random_idx:]
-
-
-def genetic_alg_general():
+def genetic_alg_general(domain, costf, pop_size = 50, max_iter = 100, elite = 0.2, mutprob = 0.3, step = 1):
     
-    # TO BE CONTINUED
-    pass
+    def mutate(vec):
+        mutate_idx = random.randint(0, len(domain)-1)
+        if vec[mutate_idx] < domain[mutate_idx][1] and vec[mutate_idx] >= domain[mutate_idx][0]:
+            return vec[0:mutate_idx] + [vec[mutate_idx]+step] + vec[mutate_idx+1:]
+        else:
+            return vec[0:mutate_idx] + [vec[mutate_idx]-step] + vec[mutate_idx+1:]
+        
+    def crossover(r1, r2):
+        crossover_idx = random.randint(0, len(domain)-2)
+        return r1[0:crossover_idx] + r2[crossover_idx:]
+    
+    # initial population
+    pop = []
+    for i in range(pop_size):
+        vec = [random.randint(domain[j][0], domain[j][1]) for j in range(len(domain))]
+        pop.append(vec)
+        
+    num_elites = int(pop_size * elite)
+    
+    for k in range(max_iter):
+        scores = [(costf(v), v) for v in pop]
+        scores.sort()
+        print scores[0][0]
+        ranked_pop = [v for (s,v) in scores]
+        
+        pop = ranked_pop[0:num_elites]
+        
+        while len(pop) < pop_size:
+            if random.random() < mutprob:
+                idx = random.randint(0,num_elites)
+                pop.append(mutate(ranked_pop[idx]))
+            else:
+                idx1 = random.randint(0,num_elites)
+                idx2 = random.randint(0,num_elites)
+                pop.append(crossover(ranked_pop[idx1], ranked_pop[idx2]))
+                
+    return scores[0]
+            
+    
+    
