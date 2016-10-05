c1780de1e6b53e964fe4c32c1c5dc22f4aa6614e
hanhan <wuhanhan999@gmail.com>
2016-10-04 20:38:51 -0700

    UPDATTE opt_network_visualization.py

--- opt_network_visualization.py
+++ opt_network_visualization.py
@@ -9,6 +9,9 @@ Here, I removed those basic ingredients such as salt, water, pepper, onion, etc.
 '''
 
 import random
+import genetic_alg_general
+import simulated_annealing_general
+
 
 gourmet_lst = ["8 treasure porridge", "8 treasure rice puding", "red bean, barley rice soup", "spicy eggplant noodles",
                    "tomato egg soup", "tomato egg noodles", "cucumber fried rice", "tomato fried rice", "spicy tofu"]
@@ -16,7 +19,7 @@ ingredients = ["red bean", "barley rice", "date", "black bean", "rose peanut", "
                    "eggplant", "tomato", "avocado", "tofu", "bean stick", "egg", "sesame oil", "rice", 
                    "cucumber", "carrot", "shrimp", "noodles", "mild spice"]
     
-links = [
+my_links = [
              ("8 treasure porridge", 
               ("red bean", "barley rice", "date", "black bean", "rose peanut", "dried longan", "honey", "sweet rice")),
              ("8 treasure rice puding", 
@@ -37,13 +40,13 @@ all_nodes.extend(ingredients)
 
 # count how many 2 lines are crossing based on the coordinates of their ends
 def count_cross(v):
-    coord_dct = dict([(all_nodes[i], (v[i*2], v[i*2+1])) for i in range(len(all_nodes))])
+    coord_dct = dict([(all_nodes[i], ((v[i], v[i*2+1]), (v[i*2], v[i+1]))) for i in range(len(all_nodes))])
     ct = 0
     
-    for i in range(len(links)):
-        (x1, y1), (x2, y2) = coord_dct[links[i][0]]
-        for j in range(len(links[i][1])):
-            (x3, y3), (x4, y4) = coord_dct[links[i][1][j]]
+    for i in range(len(my_links)):
+        (x1, y1), (x2, y2) = coord_dct[my_links[i][0]]
+        for j in range(len(my_links[i][1])):
+            (x3, y3), (x4, y4) = coord_dct[my_links[i][1][j]]
             
             den = float((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1))
             # 2 lines are parallel when den is 0
@@ -59,13 +62,18 @@ def count_cross(v):
 
 
 def main():
-    test_lst = []
-    for i in range(len(all_nodes)*2+2):
-        test_lst.append((random.randint(1, 20), random.randint(1, 20)))
+    domain = [(10, 410)]*(len(all_nodes)*2+2)
     
-    ct = count_cross(test_lst)
-    print ct
+    # genetic alg 
+    optimal_solution1 = genetic_alg_general.genetic_alg_general(domain, count_cross)
+    print optimal_solution1
     
+    # simulated annealing
+    optimal_solution2 = simulated_annealing_general.simulated_annealing(domain, count_cross)
+    print optimal_solution2
     
+    ## it seems that genetic algorithm works much better in this case
+    
+    # TO BE CONTINUED...
 if __name__ == '__main__':
     main()
