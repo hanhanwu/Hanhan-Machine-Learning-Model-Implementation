5aa0d1a0f9de9543b560563434bff163d64a9603
hanhan <wuhanhan999@gmail.com>
2016-10-26 19:36:56 -0700

    update my_decision_tree.py

--- my_decision_tree.py
+++ my_decision_tree.py
@@ -4,6 +4,8 @@ Created on Oct 24, 2016
 I'm trying to implement a decision tree,
 since it is an easy but useful classification tool, understanding its basic algorithm is helpful
 CART - chooses the best variable to divide up the data in each step
+In order to choose the best variable each step, the divided 2 sets should have least mixed situations
+To measure how mixed a set is, we use Gini Impurity or Entropy
 '''
 
 class decision_node:
@@ -29,4 +31,29 @@ def divide_data(rows, col, value):
     return st1, st2
 
 
-# TO BE CONTINUED...
+# for each set of data, count distinct labels, to help find the best variable
+def count_label(rows):
+    labels = {}
+    for r in rows:
+        l = r[-1]
+        labels.setdefault(l, 0)
+        labels[l] += 1
+    return labels
+
+
+# Variable Choose Measure 1 - Gini Impurity, the probability of putting an item to the wrong set
+def gini_impurity(rows):
+    lbs = count_label(rows)
+    total_items = len(rows)
+    gi = 0
+    for l1, ct1 in lbs.items():
+        p1 = float(ct1)/total_items
+        for l2, ct2 in lbs.items():
+            if l1 == l2: continue
+            p2 = float(l2)/total_items
+            gi += p1*p2
+            
+    return gi
+        
+
+# TO BE CONTINUED....
